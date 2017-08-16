f1607d127024fcaf39c54919a5df55b823cc4fe8
Fabio mayorga <fabio.mayorga@leadis.co>
2017-08-16 15:08:15 -0500

    credenciales

---
+++ public/credentials/credential.js
@@ -0,0 +1,8 @@
+ var config = {
+   apiKey: "AIzaSyB-4hesz35Tet9JeJVObucZJlBnT69797s",
+    authDomain: "fptc-test.firebaseapp.com",
+    databaseURL: "https://fptc-test.firebaseio.com",
+    projectId: "fptc-test",
+    storageBucket: "fptc-test.appspot.com",
+    messagingSenderId: "406095449018"
+  };
